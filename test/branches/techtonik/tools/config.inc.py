53061cb6221d2fd511e387ddc17e4a5e3d2e18b5
fargroup <fargroup@c1b7062c-c171-47ca-83b1-3db94eb7eadf>
2008-01-26 14:04:04 +0000

    + alternative enc build toolchain
    git-svn-id: http://svn.code.sf.net/p/farmanager/code/trunk@1467 c1b7062c-c171-47ca-83b1-3db94eb7eadf

---
+++ test/branches/techtonik/tools/config.inc.py
@@ -0,0 +1,15 @@
+
+from os.path import join, dirname
+
+DEST            = join(dirname(__file__), "..", "build")
+DEST_CHM        = join(DEST, "chm")
+
+#DEST_INET       = join(DEST, "inet")
+#DEST_INET_RU    = join(DEST_INET, "ru")
+#DEST_INET_EN    = join(DEST_INET, "en")
+#DEST_CHM_RU     = join(DEST_CHM, "ru")
+#DEST_CHM_EN     = join(DEST_CHM, "en")
+
+#META_RU            = join(DEST_CHM_RU, "meta")
+#META_EN            = join(DEST_CHM_EN, "meta")
+
---
+++ test/branches/techtonik/tools/untranslated.py
@@ -0,0 +1,46 @@
+# -*- coding:windows-1251 -*-
+"""
+   Recursively scan specified directory to locate files with untranslated
+   russian strings. Lines with <!-- NLC --> tag are ignored. Encyclopedia
+   image files (.gif) are also ignored.
+
+   Example:
+      untranslated.py ../enc_enc
+"""
+
+import codecs
+import os
+import re
+import sys
+
+#print "debug на русском".decode("cp1251")
+
+if len(sys.argv) < 2:
+  print __doc__
+  print
+  print "Usage: program <dir>"
+  sys.exit()
+
+
+if not os.path.exists(sys.argv[1]):
+  sys.exit("Specified path not found: %s" % sys.argv[1])
+
+ru = re.compile("[а-€ј-я] *")
+skip_mark = "<!-- NLC -->"
+for root,dirs,files in os.walk(sys.argv[1]):
+  if ".svn" in dirs:
+    dirs.remove(".svn")
+  for f in files:
+    if f.endswith(".gif"):
+      continue
+    rucount = 0
+    for l in open(os.path.join(root, f), "r"):
+      if l.find(skip_mark) != -1:
+        continue
+      rutext = "".join(ru.findall(l))
+      rucount += len(rutext)
+      #if rutext: print rutext #.decode("cp1251")
+    if rucount:
+      print "%s - %d russian symbols" % (os.path.join(root, f), rucount)
+      
+  #print root,dirs,files
