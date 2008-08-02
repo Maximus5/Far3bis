ff6fad5d81029d7cac2b8c937bcf4f7253d4f9d2
drkns <drkns@c1b7062c-c171-47ca-83b1-3db94eb7eadf>
2008-08-02 11:48:36 +0000

    All sdk-related stuff moved to special header, removed obsolete code, some cosmetics changes
    git-svn-id: http://svn.code.sf.net/p/farmanager/code/trunk@1920 c1b7062c-c171-47ca-83b1-3db94eb7eadf

--- unicode_far/headers.cpp.cpp
+++ unicode_far/headers.cpp.cpp
@@ -1,4 +1,9 @@
 /*
+headers.cpp.cpp
+
+C++ PCH
+*/
+/*
 Copyright (c) 1996 Eugene Roshal
 Copyright (c) 2000 Far Group
 All rights reserved.
@@ -28,59 +33,3 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "headers.hpp"
 #pragma hdrstop
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void *__cdecl xf_malloc(size_t __size);
-
-#ifdef __cplusplus
-}
-#endif
-
-/***
-*char *_strdup(string) - duplicate string into malloc'd memory
-*
-*Purpose:
-*       Allocates enough storage via malloc() for a copy of the
-*       string, copies the string into the new memory, and returns
-*       a pointer to it.
-*
-*Entry:
-*       char *string - string to copy into new memory
-*
-*Exit:
-*       returns a pointer to the newly allocated storage with the
-*       string in it.
-*
-*       returns NULL if enough memory could not be allocated, or
-*       string was NULL.
-*
-*Uses:
-*
-*Exceptions:
-*
-*******************************************************************************/
-
-char * __cdecl xf_strdup (const char * string)
-{
-  if(string)
-  {
-    char *memory;
-    if((memory = xf_malloc(strlen(string) + 1)) != NULL)
-       return strcpy(memory,string);
-  }
-  return(NULL);
-}
-
-wchar_t * __cdecl xf_wcsdup (const wchar_t * string)
-{
-  if(string)
-  {
-    wchar_t *memory;
-    if((memory = xf_malloc((wcslen(string)+1)*sizeof(wchar_t))) != NULL)
-       return wcscpy(memory,string);
-  }
-  return(NULL);
-}
