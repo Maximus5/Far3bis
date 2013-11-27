b95276616d051b1ba1a20d2b880876e1be3f60da
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-11-27 10:55:43 +0400

    Far3bis. headers.hpp. Debug assertions

--- unicode_far/common.hpp
+++ unicode_far/common.hpp
@@ -88,10 +88,18 @@ inline void clear_and_shrink(T& container)
 	T().swap(container);
 }
 
+#if 1
+//Maxmus: Для отлова ошибок - кидаем ассерты
+template <typename T>
+bool CheckNullOrStructSize(const T* s) {_ASSERTE(!s || (s->StructSize >= sizeof(T))); return !s || (s->StructSize >= sizeof(T));}
+template <typename T>
+bool CheckStructSize(const T* s) {_ASSERTE(!s || (s->StructSize >= sizeof(T))); return s && (s->StructSize >= sizeof(T));}
+#else
 template <typename T>
 bool CheckNullOrStructSize(const T* s) {return !s || (s->StructSize >= sizeof(T));}
 template <typename T>
 bool CheckStructSize(const T* s) {return s && (s->StructSize >= sizeof(T));}
+#endif
 
 template <typename type_1, typename type_2>
 struct simple_pair
